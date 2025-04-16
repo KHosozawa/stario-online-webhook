6ead74ae0bdf1973f195e409d40535e1104b7763
Kazumasa Hosozawa <k-hoso@star-m.jp>
2025-04-16 10:40:04 +0900

    リクエストボディのJSONをconsole.log出力するよう修正

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 
 app.post('/', function(req, res) {
-    console.log('Hello, world');
-    return res.send('Hello, world');
+    console.log(req.body);
+    return res.send();
 });
 
 app.listen(3000);
